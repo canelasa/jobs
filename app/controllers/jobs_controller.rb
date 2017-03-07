49c03a2503bfd3002b5f77e77b01a5bfb1e839a2
Anna Canelas <canelasa2@gmail.com>
2017-03-07 03:11:05 +0000

    Create edit page and make update button work

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,6 +17,20 @@ class JobsController < ApplicationController
     end
   end
 
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to job_path
+  end
+  
   private
 
   def job_params
