6358a26ee5054cdf91aef195def45a15b7a0d025
Anna Canelas <canelasa2@gmail.com>
2017-03-07 16:44:50 +0000

    Edit user permissions to edit and destroy buttons

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
      @jobs = Job.all
   end
@@ -23,16 +23,28 @@ class JobsController < ApplicationController
 
   def edit
     @job = Job.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
     @job = Job.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
     @job.update_attributes(job_params)
     redirect_to job_path
   end
 
   def destroy
     @job = Job.find(params[:id])
+    if @job.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
     @job.destroy
     redirect_to jobs_path
   end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,7 @@
 class Job < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :title, presence: true
+  validates :city, presence: true
+  validates :description, presence: true
 end
